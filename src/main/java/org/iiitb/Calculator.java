67f752456e5f81df02c10d8b31097a23a7e2d869
iamosl <osl.lavangad@gmail.com>
2022-04-17 13:21:01 +0530

    added scanner for calculator functionality

--- src/main/java/org/iiitb/Calculator.java
+++ src/main/java/org/iiitb/Calculator.java
@@ -1,5 +1,6 @@
 package org.iiitb;
 import java.lang.Math;
+import java.util.Scanner;
 
 public class Calculator {
     public static double sqRoot(double input){
@@ -26,5 +27,66 @@ public class Calculator {
         double out = Math.pow(inputX, inputY);
         return out;
 
+    }
+
+    public static void main(String[] args)
+    {
+
+        boolean flag = true;
+        Scanner in = new Scanner(System.in);
+        while (flag)
+        {
+            System.out.println("Please enter the number corresponding to the operation that you want to execute ... ");
+            System.out.println("1. Square Root");
+            System.out.println("2. Factorial");
+            System.out.println("3. Natural Logarithm");
+            System.out.println("4. Power Function");
+            System.out.println("5. Exit");
+
+            int choice = in.nextInt();
+            switch (choice)
+            {
+                case 1:
+                    {
+                        System.out.println("Input the number ...");
+                        double number = in.nextDouble();
+                        System.out.println("The Square root of " + number + " is " + sqRoot(number));
+                        break;
+                    }
+                case 2:
+                    {
+                        System.out.println("Input the number ...");
+                        int number = in.nextInt();
+                        System.out.println("The Factorial of " + number + " is " + factorial(number));
+                        break;
+                    }
+                case 3:
+                    {
+                        System.out.println("Input the number ...");
+                        double number = in.nextDouble();
+                        System.out.println("The natural log of " + number + " is " + naturalLog(number));
+                        break;
+                    }
+                case 4:
+                    {
+                        System.out.println("Input the base value ...");
+                        double base = in.nextDouble();
+                        System.out.println("Input the exponent value ...");
+                        double exponent = in.nextDouble();
+                        System.out.println("The value of " + base + " raised to the power of " + exponent + " is " + power(base, exponent));
+                        break;
+                    }
+                case 5:
+                    {
+                        flag = false;
+                        System.out.println("Thank you! Exiting now ...");
+                        break;
+                    }
+                default:
+                    {
+                        System.out.println("Invalid Choice .. Please Try Again");
+                    }
+            }
+        }
     }
 }
